5c574ebc29ab229cd5bf35d54228435349641e9b
Elodie <43603718+ElodieLam@users.noreply.github.com>
2023-08-21 17:27:41 +0200

    Create Back_up_services.py

---
+++ Back_up_services.py
@@ -0,0 +1,206 @@
+import sys
+import json
+from f_http import *
+import getopt
+
+#================================================
+# Variables
+#================================================
+
+### Environment managed
+ENV=os.getenv('env') #ENV = DEV | RE7 | OPE
+Cluster=os.getenv('Cluster')
+ClusterToken=os.getenv('ClusterToken')
+GCPTenant=Cluster+str(os.getenv('GCPTenant'))
+GCPToken=os.getenv('GCPToken')
+AWSTenant=Cluster+str(os.getenv('AWSTenant'))
+AWSToken=os.getenv('AWSToken')
+INTRATenant=Cluster+str(os.getenv('INTRATenant'))
+INTRAToken=os.getenv('INTRAToken')
+AZRTenant=Cluster+str(os.getenv('AZRTenant'))
+AZRToken=os.getenv('AZRToken')
+Managed_TENANT={GCPTenant: GCPToken, AWSTenant: AWSToken, AZRTenant: AZRToken, INTRATenant: INTRAToken}
+
+### Environment saas
+SaasTenant="https://"+str(os.getenv('SaasTenant'))
+SaasToken=os.getenv('SaasToken')
+
+### Mapping
+Api ={'SETTINGS_OBJECT' : '/api/v2/settings/objects', 'ENTITY': '/api/v2/entities/'}
+schemaIds={'KEY_REQUESTS':'builtin:settings.subscriptions.service','MUTED_REQUESTS':'builtin:settings.mutedrequests', 'HTTP_FAILURE':'builtin:failure-detection.service.http-parameters', 'GENERAL_FAILURE': 'builtin:failure-detection.service.general-parameters'}
+
+
+#================================================
+# Functions
+#================================================
+def get_entity(url, token, api, entityId, option):
+    url = url + api + "/" + entityId + option 
+    error, message = http_get(url, token)
+    check_for_errors(error, message)
+    return error, message
+
+def get_entity(url, token, api, entityId):
+    url = url + api + "/" + entityId  
+    error, message = http_get(url, token)
+    check_for_errors(error, message)
+    return error, message
+
+def get_settings_object(url, token, api, schemaIds, fields):
+    schemaIds =  schemaIds.replace(":", "%3A")
+    schemaIds =  schemaIds.replace(",", "%2C")
+    url = url + api + "?schemaIds=" + schemaIds + "&fields=" + fields + "&pageSize=500&author"
+    error, message = http_get(url, token)
+    check_for_errors(error, message)
+    return error, message
+
+#================================================
+# Main functions
+#================================================
+
+#Backup service settings object
+def backup_settings (url, token, api, schema, filename):
+    objects=[]
+    errors=[]
+    
+    print("Result in: " + filename)
+
+    #0-Create backup file
+    with open(filename, "w") as f: 
+        cpt = 0
+        
+        #1-Get Settings object
+        error, settings_object = get_settings_object(url, token, api, schema,"scope,value")
+        if error:
+            print("Error - Get settings object")
+            errors.append(schema)
+        else:
+            settings_object_json = json.loads(settings_object)
+
+            if settings_object_json['items']: #if Object is not empty 
+                for object in settings_object_json['items']: #Add each object to list
+                    cpt = cpt + 1
+                    settings_object_json = object
+                    serviceId = settings_object_json['scope']
+                    value = settings_object_json['value']
+
+                    #2-Get entity Name
+                    error,entity = get_entity(url, token, Api["ENTITY"], serviceId)
+                    entity_json = json.loads(entity)
+                    entityName = entity_json['displayName']
+
+                    #3-Get processGroupId, PGentityName and hostGroupId
+                    hostGroupIds =[]
+
+                    if entity_json['fromRelationships']:
+                        processGroupId = entity_json['fromRelationships']['runsOn'][0]['id']
+                        error,entity = get_entity(url, token, Api["ENTITY"], processGroupId)
+                        entity_json = json.loads(entity)
+                        PGentityName = entity_json['displayName']
+
+                        if entity_json['fromRelationships']['runsOn'][0]["id"]:
+                            list_hosts = entity_json['fromRelationships']['runsOn']
+                            for h in list_hosts:
+                                error,host = get_entity(url, token, Api["ENTITY"], h["id"])
+                                host_json = json.loads(entity)
+                                if "isInstanceOf" in host_json["fromRelationships"]:
+                                    hostGroupIds.append(host_json["fromRelationships"]["isInstanceOf"]["id"])
+                                else:
+                                    continue
+
+                        #4-Create json structure for the backup
+                        object = {
+                                "schemaId" : schema,
+                                "scope" : serviceId,
+                                "value" : value,
+                                "entityName" : entityName,
+                                "PROCESS_GROUP_entityName": PGentityName,
+                                "HOST_GROUP_ID": hostGroupIds,
+                                "status":"to import"
+                            }
+                        
+                        #5-Add result to list and create csv mapping
+                        objects.append(object) 
+                        print(entityName + ";" + serviceId + ";" + PGentityName  + ";" + processGroupId) 
+
+                    else:
+                        #4 bis-Create json structure for the backup if error
+                        object = {
+                                "schemaId" : schema,
+                                "scope" : serviceId,
+                                "value" : value,
+                                "entityName" : entityName,
+                                "PROCESS_GROUP_entityName": "fromRelationships empty",
+                                "HOST_GROUP_ID": "fromRelationships empty",
+                                "status":"to import"
+                            }
+                        
+                        #5 bis-Add result to list and create csv mapping
+                        objects.append(object) 
+                        print(entityName + ";" + serviceId)       
+                     
+            else:
+                key_request_list = {}
+        
+        #6-Write result into file
+        json.dump(objects, f, indent=4) 
+        f.close()
+        print("Done writing JSON data into file.") 
+        print("Count: ")
+        print(cpt)
+        print("Settings object not backed up: ")
+        print(errors)
+
+def main():
+
+    #Get arguments
+    try: 
+        optlist, args = getopt.getopt(sys.argv[1:], 'x', ['tenant='])
+        #print ('Argument list:') 
+        #print(optlist) 
+        #print(args)
+
+    except getopt.GetoptError as err: # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+
+    #Parse arguments
+    for arg, value in optlist:
+        if arg == '--tenant':
+            tenant = optlist[0][1] 
+
+    if tenant == "intra":
+        ManagedTenant = INTRATenant
+        ManagedToken = INTRAToken
+    elif tenant == "aws":
+        ManagedTenant = AWSTenant
+        ManagedToken = AWSToken
+    elif tenant == "gcp":
+        ManagedTenant = GCPTenant
+        ManagedToken = GCPToken
+    elif tenant == "azr":
+        ManagedTenant = AZRTenant
+        ManagedToken = AZRToken
+    else:
+        print("Error - Wrong argument")
+        sys.exit(1)
+
+    print("\n//---Key requests--//")
+    filename = "backup_service_key_requests-" + ENV.lower() + "-" + tenant + ".json" 
+    backup_settings (ManagedTenant, ManagedToken, Api["SETTINGS_OBJECT"], schemaIds['KEY_REQUESTS'], filename)
+    
+    print("\n//---Muted requests--//")
+    filename = "backup_service_muted_requests-" + ENV.lower() + "-" + tenant + ".json" 
+    backup_settings (ManagedTenant, ManagedToken, Api["SETTINGS_OBJECT"], schemaIds['MUTED_REQUESTS'], filename)
+
+    print("\n//---HTTP failure detection--//")
+    filename = "backup_service_http_failure_detection-" + ENV.lower() + "-" + tenant + ".json" 
+    backup_settings (ManagedTenant, ManagedToken, Api["SETTINGS_OBJECT"], schemaIds['HTTP_FAILURE'], filename)
+
+    print("\n//---General failure detection--//")
+    filename = "backup_service_general_failure_detection-" + ENV.lower() + "-" + tenant + ".json" 
+    backup_settings (ManagedTenant, ManagedToken, Api["SETTINGS_OBJECT"], schemaIds['GENERAL_FAILURE'], filename)
+    
+    
+if __name__ == '__main__':
+    main()
+
+#__END__
